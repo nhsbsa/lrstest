5b7388a16e4842a2e8de453f2cd67a9010f195c6
Tim Gorringe <tgorringe4@gmail.com>
2018-10-16 09:36:34 +0100

    removed everything but HC3

--- app/routes.js
+++ app/routes.js
@@ -32,19 +32,6 @@ router.get(/index/, function (req, res) {
     console.log(hasBen);
 });
 
-router.get('/challenge/verify/attempt-handler/', function (req, res) {
-    
-    var result = attempt++;
-    
-    console.log(result);
-    
-  if (result == 2) {
-    res.redirect('fail');
-  }
- else {
-    res.redirect('nextGo');
-  }
-});
 
 router.get('/', function (req, res) {
   attempt = 0;
@@ -54,77 +41,6 @@ router.get('/', function (req, res) {
 router.get(/index/, function (req, res) {
   attempt = 0;
   res.render('index');
-});
-
-router.get(/instalment-handler/, function (req, res) {
-  if (req.query.plan == "2") {
-    account.instalments = 6;
-    plan = 172;
-  } else {
-    account.instalments = 12;
-    plan = 86;
-  }
-  res.redirect('dates');
-});
-
-router.get('/payments/dd/contact-handler/', function (req, res) {
-  hasText = false;
-  hasEmail = false;
-  if (req.query.text == 'true') {
-    hasText = true;
-  }
-  if (req.query.email == 'true') {
-    hasEmail = true;
-  }
-  if (hasText) {
-    res.redirect('text-value');
-  } else if (hasEmail) {
-     res.redirect('mail-value');
-  } else {
-    hasText = false;
-    hasEmail = false;
-    res.redirect('accept');
-  }
-});
-
-router.get('/payments/rp/contact-handler/', function (req, res) {
-  hasText = false;
-  hasEmail = false;
-  if (req.query.text == 'true') {
-    hasText = true;
-  }
-  if (req.query.email == 'true') {
-    hasEmail = true;
-  }
-  if (hasText) {
-    res.redirect('text-value');
-  } else if (hasEmail) {
-     res.redirect('mail-value');
-  } else {
-    hasText = false;
-    hasEmail = false;
-    res.redirect('accept');
-  }
-});
-
-router.get('/payments/so/contact-handler/', function (req, res) {
-  hasText = false;
-  hasEmail = false;
-  if (req.query.text == 'true') {
-    hasText = true;
-  }
-  if (req.query.email == 'true') {
-    hasEmail = true;
-  }
-  if (hasText) {
-    res.redirect('text-value');
-  } else if (hasEmail) {
-     res.redirect('mail-value');
-  } else {
-    hasText = false;
-    hasEmail = false;
-    res.redirect('accept');
-  }
 });
 
 router.get(/haha-handler/, function (req, res) {
